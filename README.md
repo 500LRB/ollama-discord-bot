8f2b294c719c29dc25ec43ede7cbe948609a27ad
500LRB <loganb605@gmail.com>
2025-02-18 20:16:22 -0600

    i love this discord bot

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 silly ai bot that uses ollama and discord.py to talk when you ping it 
 
 # dependencies
-1. llama3.2 (from ollama)
+1. the model of your choice (change the variable `model` with your model)
 2. discord.py
 3. asyncio
-4. ollama (for python)
+4. ollama python module
+5. base64 (should be included in python)
+
+# features
+1. responds to reply pings and regular pings
+2. can remember stuff, memories/history is for specific channels rather than global
+3. image support (if the model supports it)
+4. open source!
+5. extremely simple, !clear to clear channel history (thats the only command)
+6. responds to other bots! (i wouldnt call it a feature but more rather a thing that im too lazy to fix)
--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ import base64
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-model = 'llama3.2-vision'
+model = 'llama3.2-vision' # the model you'd like to use for ollama
+new_message_delay = 0.05 # in discord, if a message is over 4000 characters, this is for the delay of sending the split up text
+enable_vision = True
 
 pinged_messages = {}
 
@@ -17,7 +19,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == "!clear":
+    if message.content == "!clear": #used to clear the channel memory/history
         async with message.channel.typing():
             if message.channel.id in pinged_messages:
                 pinged_messages[message.channel.id].clear()
@@ -25,18 +27,18 @@ async def on_message(message):
 
     elif client.user.mentioned_in(message):
 
-        if message.attachments:
+        if message.attachments and enable_vision: #stuff used for the images
             image = []
-            for chunk in range(0, len(message.attachments)):
+            for chunk in range(0, len(message.attachments)): #some weird implementation i had to do to get discord.py to read multiple attachments
                 attachment = message.attachments[chunk]
                 attachment_content = await message.attachments[chunk].read()
-                print(message.attachments)
-                print(attachment)
-                print(attachment.content_type)
+
+                print(f"message has {len(message.attachments)} amount of attachments")
+                
                 if attachment.content_type.startswith("image/"):
                     image.append(base64.b64encode(attachment_content).decode("utf-8"))
                 else:
-                    return()
+                    print(f"unable to append attachment {chunk} as it isnt an image, instead is a {attachment.content_type}")
         else:
             image = []
 
@@ -63,13 +65,13 @@ async def on_message(message):
                         "role": "assistant",
                         "content": response.message.content
                     })
-                print(pinged_messages[message.channel.id])
+                print(f"message: {msg} sent by {message.author}\n AI response: {response.message.content}")
                 if len(response.message.content) > 4000:
                     chunks = [response.message.content[i:i+4000 ] for i in range(0, len(response.message.content), 4000 )]
-                    print(f"chunks made {len(chunks)}")
+                    print(f"chunks made for this message is {len(chunks)}")
                     for chunk in chunks: 
                         await message.reply(chunk)
-                        await asyncio.sleep(0.05)
+                        await asyncio.sleep(new_message_delay)
                 else:
                     await message.reply(response.message.content)
         except Exception as e:
