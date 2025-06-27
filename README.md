da9c03f909965f707fd94a00fb66031d44de6a9f
500LRB <loganb605@gmail.com>
2025-06-27 10:45:08 -0500

    i made a lot of changes, or well i consider it a lot

--- README.md
+++ README.md
@@ -2,16 +2,19 @@
 silly ai bot that uses ollama and discord.py to talk when you ping it 
 
 # dependencies
-1. the model of your choice (change the variable `model` with your model)
 2. discord.py
-3. asyncio
+3. asyncio (included with python)
 4. ollama python module
 5. base64 (should be included in python)
 
+# how to do shiz!!
+1. change model of your choice (change the variable `model` with your model)
+2. you can make the bot respond to everything AND choose which servers it will respond to, including channels!!
+3. insert shiz here1!!1
+
 # features
 1. responds to reply pings and regular pings
-2. can remember stuff, memories/history is for specific channels rather than global
+2. can remember stuff, memories/history is for specific channels rather than globally
 3. image support (if the model supports it)
-4. open source!
 5. extremely simple, !clear to clear channel history (thats the only command)
 6. responds to other bots! (i wouldnt call it a feature but more rather a thing that im too lazy to fix)
--- bot.py
+++ bot.py
@@ -2,14 +2,15 @@ import discord
 import asyncio
 from ollama import AsyncClient
 import base64
-import os
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 model = 'llama3.2-vision' # the model you'd like to use for ollama
 new_message_delay = 0.05 # in discord, if a message is over 2000 characters, this is for the delay of sending the split up text
 enable_vision = True
-respond_all = False #this makes it respond to EVERYTHING
+respond_all = True #this makes it respond to EVERYTHING
+respond_all_servers = [] #use this in conjunction with respond_all to make it only respond to certain servers, leave it empty to respond to all servers
+respond_all_channels = [] #use this in conjunction with respond_all and respond_all_servers to make it only respond to certain channels, leave it empty to respond to all channels in the server
 
 pinged_messages = {}
 
@@ -20,27 +21,36 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author is client.user: 
-        return #doesnt respond to itself
-    elif client.user is None: 
-        return
-    elif message.content == "!clear": #used to clear the channel memory/history
+    if message.author.id is client.user.id: return #doesnt respond to itself
+    elif client.user is None: return #too lazy to remove, and i dont think this works anyway
+
+    if message.content == "!clear": #used to clear the channel memory/history
         async with message.channel.typing():
             if message.channel.id in pinged_messages:
-                pinged_messages[message.channel.id] = []
+                pinged_messages[message.channel.id].clear()
                 await message.channel.send(f"message history cleared for channel id {message.channel.id}")
-
-    elif client.user.mentioned_in(message) or respond_all:
+            else:
+                await message.channel.send(f"nothing to clear!")
+        return
+    if respond_all and not client.user.mentioned_in(message) and (
+        (respond_all_servers and message.guild.id not in respond_all_servers) or        
+        (
+            (respond_all_servers and message.guild.id in respond_all_servers) and 
+            (respond_all_channels and message.channel.id not in respond_all_channels)
+        )):
+            return
+    if client.user.mentioned_in(message) or respond_all:
         msg = message.content.replace(f"<@{client.user.id}>", "").strip()
         image = []
+
         if message.channel.id not in pinged_messages:
             pinged_messages[message.channel.id] = []
 
         if message.attachments and enable_vision: #stuff used for the images
             for attachment in message.attachments: #some weird implementation i had to do to get discord.py to read multiple attachments
                 attachment_content = await attachment.read()
-                print(f"message has {len(message.attachments)} amount of attachments")
-                
+                print(f"message has {len(message.attachments)} attachments")
+
                 if attachment.content_type.startswith("image/"):
                     image.append(base64.b64encode(attachment_content).decode("utf-8"))
                 else:
@@ -64,13 +74,17 @@ async def on_message(message):
                         "role": "assistant",
                         "content": response.message.content
                     })
-                print(f"message: {msg} sent by {message.author}\nAI response: {response.message.content}")
+                print(f"message: {msg} (sent by {message.author.id})\nAI response: {response.message.content}")
 
                 if len(response.message.content) > 2000: #sending messages and stuff
                     characters_left = 2000
                     message_split = response.message.content.split(' ')
                     last_i = 0
                     chunks = []
+                    should_reply = 1
+                    print(f"message is over 2000 characters, splitting into chunks")
+                    print(f"message length is {len(response.message.content)}")
+                    
                     for i, word in enumerate(message_split):
                         if (len(word)+1) > characters_left:
                             words = ' '.join(message_split[last_i:i])
@@ -80,15 +94,21 @@ async def on_message(message):
                         else:
                             characters_left -= (len(word) + 1)
                     chunks.append(' '.join(message_split[last_i:]))
+
                     print(f"chunks made for this message is {len(chunks)}")
+
                     for messages in chunks: 
-                        await message.reply(messages)
+                        if should_reply > 1:
+                            await message.channel.send(messages)
+                        else:
+                            await message.reply(messages)
+                            should_reply += 1
                         await asyncio.sleep(new_message_delay)
                 else:
                     await message.reply(response.message.content)
 
         except Exception as e:
             print(f"Error: {e}")
-            await message.reply("`something went wrong (please dm me to fix)`")
+            await message.reply(f"Error: {e}, something went wrong!1!1!!")
 
-client.run(os.environ['BOT_TOKEN'])
+client.run(open("token.txt", "r").read())
